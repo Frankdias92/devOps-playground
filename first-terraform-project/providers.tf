4f78c8bae41c831a5584b50d5b458d62d6dcf205
Frankdias92 <frankmcdias@gmail.com>
2024-12-27 09:40:06 -0300

    :sparkles: Feat: set variables tf

--- first-terraform-project/providers.tf
+++ first-terraform-project/providers.tf
@@ -5,9 +5,33 @@ terraform {
       version = "5.82.2"
     }
   }
+  backend "s3" {
+    bucket = "franklin-state-bucket-tf"
+    region = "us-west-2" 
+    key = "terraform.tfstate"
+    encrypt = true
+    profile = "Franklin"
+  }
 }
 
 provider "aws" {
   profile = "Franklin"
   region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [ aws_s3_bucket.terraform_state ]
 }
---
+++ first-terraform-project/variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type = string
+  default = "franklin-state-bucket-tf"
+  description = "Bucket with state"
+}
