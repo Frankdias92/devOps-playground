e6f84fb6f9634703ab15597e76a363b9e051e886
Frankdias92 <frankmcdias@gmail.com>
2024-12-29 15:15:42 -0300

    :bug: Fix: update terraform files to fix OIDC

--- ci-cd/iac/ecr.tf
+++ ci-cd/iac/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "franklin-ci-api" {
-  name                 = "franklin-ci"
+  name                 = "franklin-ci-api"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
